06567621e41cfbb580c9af87db1b733fcff9da81
0xSh4dy <84446794+0xSh4dy@users.noreply.github.com>
2021-09-26 08:17:18 +0530

    Update tokenCreator.py

--- xplore/tokenCreator.py
+++ xplore/tokenCreator.py
@@ -1,6 +1,6 @@
 from hashlib import sha1
 from datetime import datetime, time
-def generateToken(email):
+def generateToken(email): 
     timeStamp = datetime.now()
     token = str(timeStamp)+email
     token = sha1(token.encode()).hexdigest()

0d7c491abc3d9c2a5ecceeb6a1a6f44f6082606e
r4ksh1t0011 <rakshitawasthi17@gmail.com>
2021-09-26 05:53:38 +0530

    new

---
+++ d
@@ -0,0 +1,6 @@
+ id | username |                             password                             |           email            | accountActive 
+----+----------+------------------------------------------------------------------+----------------------------+---------------
+  1 | ra       | 37e7702e5ce449c1225fa1b3b3ed8e2dff35d24464d9264a4c2bebf3e0deee05 | rakshitawasthi17@gmail.com | t
+  2 | mahi     | 78d7a82604de80d2b191e92a96d23a2803c0a0e910a32a6744e3ecd41e4da4cf | awasthiirakshit@gmail.com  | t
+(2 rows)
+
--- xplore/models.py
+++ xplore/models.py
@@ -11,10 +11,10 @@ class Users(models.Model):
 #     users = models.ForeignKey(Users,on_delete=models.CASCADE)
 
 class Course(models.Model):
-    courseName = models.CharField(max_length=50,null=True)
-    coursePlatform = models.CharField(max_length=50,null=True)
-    imageUrl = models.TextField(null=True)
-    courseUrl = models.TextField(null=True)
+    coursename = models.CharField(max_length=50,null=True)
+    courseplatform = models.CharField(max_length=50,null=True)
+    imageurl = models.TextField(null=True)
+    courseurl = models.TextField(null=True)
     rating = models.IntegerField(null=True)
     # subscribers = models.IntegerField(null=True)
     details = models.TextField(null=True)    
